93c118f7d71b45363f60ac360d9fa6ed4d63b768
Tiago Caires Carvalho <tiagocaires.tcc@gmail.com>
2018-01-14 23:55:07 -0300

    Migration ImageRepo

--- src/Action/Migrations/20180115025406_Imagerepo.Designer.cs
+++ src/Action/Migrations/20180115025406_Imagerepo.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Action.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180115025406_Imagerepo")]
+    partial class Imagerepo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Action/Migrations/20180115025406_Imagerepo.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Action.Migrations
+{
+    public partial class Imagerepo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    Base64Image = table.Column<string>(type: "longtext", maxLength: 2147483647, nullable: true),
+                    ImageName = table.Column<string>(type: "longtext", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
