aeab265b65b8737f1b5b21499c482a6bde1401f7
Tiago Caires Carvalho <tiagocaires.tcc@gmail.com>
2018-01-15 20:02:36 -0300

    Migration NluResultTableId

--- src/Action/Migrations/20180115222009_NluResultTableId.Designer.cs
+++ src/Action/Migrations/20180115222009_NluResultTableId.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Action.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180115222009_NluResultTableId")]
+    partial class NluResultTableId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
