5d21727fd0975313f13d38a463280c685905ca1f
Tiago Caires Carvalho <tiagocaires.tcc@gmail.com>
2018-01-15 22:14:33 -0300

    Migration RelationTypes

--- src/Action/Migrations/20180116011301_RelationTypes.Designer.cs
+++ src/Action/Migrations/20180116011301_RelationTypes.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Action.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180116011301_RelationTypes")]
+    partial class RelationTypes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
