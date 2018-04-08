3b44d8699405e4c1c1c91547f9f98086f1785e62
luiz <luiz@nexo.ai>
2018-04-08 01:47:00 -0300

    Adjuste User/Me Response Model

--- src/Action/VewModels/UserViewModel.cs
+++ src/Action/VewModels/UserViewModel.cs
@@ -1,14 +1,24 @@
-﻿namespace Action.VewModels
+﻿using Newtonsoft.Json;
+
+namespace Action.VewModels
 {
 	public class UserViewModel
 	{
+		[JsonProperty("id")]
 		public string Id { get; set; }
+		[JsonProperty("accountId")]
 		public int AccountId { get; set; }
+		[JsonProperty("role")]
 		public string Role { get; set; }
+		[JsonProperty("email")]
 		public string Email { get; set; }
+		[JsonProperty("name")]
 		public string Name { get; set; }
+		[JsonProperty("surname")]
 		public string SurName { get; set; }
+		[JsonProperty("phone")]
 		public string Phone { get; set; }
+		[JsonProperty("administrator")]
 		public bool Administrator { get; set; }
 	}
 }
