5d80b61670625688cf0b7d57a1ff44a247929e53
luiz <luiz@nexo.ai>
2018-04-08 02:24:40 -0300

    Adjuste Account Response Model

--- src/Action/VewModels/AccountViewModel.cs
+++ src/Action/VewModels/AccountViewModel.cs
@@ -1,17 +1,25 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Action.VewModels
 {
 	public class AccountViewModel
 	{
+		[JsonProperty("id")]
 		public int Id { get; set; }
+		[JsonProperty("companyName")]
 		public String CompanyName { get; set; }
+		[JsonProperty("plan")]
 		public PlanViewModel Plan { get; set; }
+		[JsonProperty("secondaryPlans")]
 		public List<SecondaryPlanViewModel> SecondaryPlans { get; set; } = new List<SecondaryPlanViewModel>();
+		[JsonProperty("entities")]
 		public List<EntityViewModel> Entities { get; set; } = new List<EntityViewModel>();
+		[JsonProperty("users")]
 		public List<UserViewModel> Users { get; set; } = new List<UserViewModel>();
+		[JsonProperty("status")]
 		public string Status { get; set; }
 	}
 }
