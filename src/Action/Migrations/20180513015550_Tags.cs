b924bce149884f24ee76ae719ede1e7b4239bd0c
luizcarvalho <luizhmcarvalho@gmail.com>
2018-05-12 23:01:56 -0300

    Normatize Json Properties

---
+++ src/Action/Migrations/20180513015550_Tags.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Action.Migrations
+{
+    public partial class Tags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BriefingTag",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Text = table.Column<string>(maxLength: 180, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BriefingTag", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EntityRole",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 180, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntityRole", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BriefingTag");
+
+            migrationBuilder.DropTable(
+                name: "EntityRole");
+        }
+    }
+}
