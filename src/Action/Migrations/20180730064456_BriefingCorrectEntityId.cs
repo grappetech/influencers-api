c08309532d45cd701baf5a1afab8733b34e41aa6
Luiz Carvalho <luiz@nexo.ai>
2018-08-14 00:00:26 -0300

    T

---
+++ src/Action/Migrations/20180730064456_BriefingCorrectEntityId.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Action.Migrations
+{
+    public partial class BriefingCorrectEntityId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ConnectedEntityId",
+                table: "Briefings",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Briefings_ConnectedEntityId",
+                table: "Briefings",
+                column: "ConnectedEntityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Briefings_Entities_ConnectedEntityId",
+                table: "Briefings",
+                column: "ConnectedEntityId",
+                principalTable: "Entities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Briefings_Entities_ConnectedEntityId",
+                table: "Briefings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Briefings_ConnectedEntityId",
+                table: "Briefings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ConnectedEntityId",
+                table: "Briefings",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldNullable: true);
+        }
+    }
+}
---
+++ src/Action/VewModels/BriefingRequestViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Action.VewModels
+{
+    public class BriefingRequestViewModel
+    {
+        
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("description")]
+            public string Description { get; set; }
+
+            [JsonProperty("gender")]
+            public string Gender { get; set; }
+
+            [JsonProperty("role")]
+            public List<string> Role { get; set; }
+
+            [JsonProperty("factor")]
+            public List<FactorViewModel> Factor { get; set; }
+
+            [JsonProperty("targetGender")]
+            public string TargetGender { get; set; }
+
+            [JsonProperty("ageRange")]
+            public string AgeRange { get; set; }
+
+            [JsonProperty("city")]
+            public string City { get; set; }
+
+            [JsonProperty("targetCity")]
+            public string TargetCity { get; set; }
+        }
+}
